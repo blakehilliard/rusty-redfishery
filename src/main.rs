1fed1da4df815d59ba86df40d873b62dd417f5f7
blakehilliard <blakesto@gmail.com>
2023-04-02 12:58:13 -0500

    Now returns proper response for a GET on /redfish

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use axum::{
     routing::get,
+    response::Json,
     Router,
 };
+use serde_json::{Value, json};
+
+async fn get_redfish() -> Json<Value> {
+    Json(json!({ "v1": "/redfish/v1/" }))
+}
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/redfish", get(get_redfish));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
