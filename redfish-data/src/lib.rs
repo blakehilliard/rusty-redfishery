69fc285f124e34eb511a6ee87543ca106d8627af
blakehilliard <blakesto@gmail.com>
2023-05-14 13:51:40 -0500

    First step toward reorganizing into workspace that will do more thoughtful separation of redfish "data" from the HTTP-specific stuff.

---
+++ redfish-data/src/lib.rs
@@ -0,0 +1,83 @@
+use std::sync::{Arc, Mutex};
+use serde_json::{Value, json};
+
+#[derive(Clone, PartialEq)]
+pub struct RedfishResourceSchemaVersion {
+    major: u32,
+    minor: u32,
+    build: u32,
+}
+
+impl RedfishResourceSchemaVersion {
+    pub fn new(major: u32, minor: u32, build: u32) -> Self {
+        Self { major, minor, build }
+    }
+
+    pub fn to_str(&self) -> String {
+        format!("v{}_{}_{}", self.major, self.minor, self.build)
+    }
+}
+
+#[derive(Clone, PartialEq)]
+pub struct RedfishCollectionSchemaVersion {
+    version: u32,
+}
+
+impl RedfishCollectionSchemaVersion {
+    pub fn new(version: u32) -> Self {
+        Self { version }
+    }
+
+    pub fn to_str(&self) -> String {
+        format!("v{}", self.version)
+    }
+}
+
+pub struct RedfishResourceType {
+    pub name: String,
+    pub version: RedfishResourceSchemaVersion,
+    xml_schema_uri: String,
+}
+
+impl RedfishResourceType {
+    // Create for a DMTF schema of a redfish resource
+    pub fn new_dmtf(name: String, version: RedfishResourceSchemaVersion) -> Self {
+        Self {
+            xml_schema_uri: format!("http://redfish.dmtf.org/schemas/v1/{}_v{}.xml", name, version.major),
+            name,
+            version,
+        }
+    }
+
+    // TODO: This should be more commonized
+    fn get_versioned_name(&self) -> String {
+        format!("{}.{}", self.name, self.version.to_str())
+    }
+
+    fn to_xml(&self) -> String {
+        format!("  <edmx:Reference Uri=\"{}\">\n    <edmx:Include Namespace=\"{}\" />\n    <edmx:Include Namespace=\"{}\" />\n  </edmx:Reference>\n",
+                self.xml_schema_uri, self.name, self.get_versioned_name())
+    }
+}
+
+pub struct RedfishCollectionType {
+    pub name: String,
+    pub version: RedfishCollectionSchemaVersion,
+    xml_schema_uri: String,
+}
+
+impl RedfishCollectionType {
+    // Create for a DMTF schema of a redfish collection
+    pub fn new_dmtf(name: String, version: RedfishCollectionSchemaVersion) -> Self {
+        Self {
+            xml_schema_uri: format!("http://redfish.dmtf.org/schemas/v1/{}_{}.xml", name, version.to_str()),
+            name,
+            version,
+        }
+    }
+
+    fn to_xml(&self) -> String {
+        format!("  <edmx:Reference Uri=\"{}\">\n    <edmx:Include Namespace=\"{}\" />\n  </edmx:Reference>\n",
+                self.xml_schema_uri, self.name)
+    }
+}
