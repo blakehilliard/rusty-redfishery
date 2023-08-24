4be931c033a8f4529025d2978abd0b708666e4bb
blakehilliard <blakesto@gmail.com>
2023-08-23 23:03:57 -0500

    Fix

--- test.py
+++ test.py
@@ -3,27 +3,27 @@
 import subprocess
 
 def get_uri(uri):
-    cmd = ["curl", "-igk", f"http://localhost:3000{uri}"]
+    cmd = ["curl", "-igk", "-u", "root:root", f"https://localhost:3000{uri}"]
     print("\n===", uri, "===")
     out = subprocess.check_output(cmd).decode()
     print(out)
     return out
 
 res = get_uri("/redfish")
-assert("200 OK" in res)
+assert("HTTP/2 200" in res)
 assert('{"v1":"/redfish/v1/"}' in res)
 
 res = get_uri("/redfish/")
-assert("200 OK" in res)
+assert("HTTP/2 200" in res)
 assert('{"v1":"/redfish/v1/"}' in res)
 
 res = get_uri("/redfish/v1/")
-assert("200 OK" in res)
+assert("HTTP/2 200" in res)
 assert('"@odata.id":"/redfish/v1"' in res)
 assert('"@odata.type":"#ServiceRoot.v1_15_0.ServiceRoot"' in res)
 assert('"Id":"RootService"' in res)
 assert('"Name":"Root Service"' in res)
 
 res = get_uri("/redfish/v1/NotFound")
-assert("404 Not Found" in res)
+assert("HTTP/2 404" in res)
 assert("content-length: 0" in res)
